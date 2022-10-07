9ff1aa6d0bd092120bc1344815ad1a2077534373
Marcos Velasquez <marcosv200007@gmail.com>
2022-10-07 17:29:39 -0600

    Arreglo de json

--- src/app/dummyData/posts.json.ts
+++ src/app/dummyData/posts.json.ts
@@ -1,8 +1,37 @@
 import { Post } from "../home/post";
 
 export const POSTS: Post[] = [
-    {id: 1, contenido: 'lorem ipsum', fecha: '2022-10-04', meGusta: [1, 2, 3], usuario: 'mael358', meGusto: true},
-    {id: 2, contenido: 'lorem ipsum 2', fecha: '2022-10-01', meGusta: [4, 5], usuario: 'hveliz', meGusto: false},
-    {id: 3, contenido: 'lorem ipsum 3', fecha: '2022-10-03', meGusta: [7, 1], usuario: 'fatima', meGusto: true},
-    {id: 5, contenido: 'lorem ipsum 4', fecha: '2022-10-27', meGusta: [1, 3, 6, 8], usuario: 'diego', meGusto: false}
+    {
+        id: 1, 
+        contenido: 'lorem ipsum', 
+        fecha: '2022-10-04', 
+        meGusta: [
+            1, 
+            2, 
+            3
+        ], 
+        usuario: {
+            id: 1,
+            foto: '/home/mael/fotos/1.jpg',
+            email: 'marcosv200007@gmail.com',
+            nombres: 'Marcos Emilio',
+            apellidos: 'Velasquez Hidalgo',
+            password: 'a24&rkj@45./asf'
+        }, 
+        meGusto: true
+    },
+    { id: 2, 
+        contenido: 'lorem ipsum 2', 
+        fecha: '2022-10-01', 
+        meGusta: [4, 5], 
+        usuario: {
+            id: 2,
+            foto: '/home/hveliz/fotos/1.jpg',
+            email: 'hveliz@gmail.com',
+            nombres: 'Harry Enrique',
+            apellidos: 'Veliz',
+            password: 'a24&rkj@45./asf'
+        }, 
+        meGusto: false 
+    }
 ]
